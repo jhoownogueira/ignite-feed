cadce00fa9e043f6ba529fe889b312ae9a0320cf
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-08-30 17:45:32 -0300

    Componente Post
    Estilização Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
+
 import './global.css';
-import { Post } from './assets/Post';
-import { Sidebar } from './components/Sidebar';
+
 
 export function App() {
   return (
