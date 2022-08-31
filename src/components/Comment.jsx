5f484fd0c253dc388579eb8f31a775d90520fe38
Jhonata Nogueira <jhonata@flashnetbrasil.com.br>
2022-08-30 21:28:50 -0300

    Post e comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/jhoownogueira.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Jhonata Nogueira</strong>
+                            <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Jhonatan, parabéns 👏👏</p>
+
+                    <footer>
+                        <button>
+                            <ThumbsUp />
+                            Aplaudir <span>20</span>
+                        </button>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    )
+}
