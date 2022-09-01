b1ec571e44f9b35a6f882a7764e2db8969c75ed8
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-09-01 16:44:05 -0300

    Iterando componente post e inserindo propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,51 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/jhoownogueira.png',
+      name: 'Jhonata Nogueira',
+      role: 'Front-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/lgustavs.png',
+      name: 'Luiz Gustavo',
+      role: 'Front-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://github.com/DanielFerreiraOP.png',
+      name: 'Daniel Ferreira',
+      role: 'DevOps',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+];
+
 
 export function App() {
   return (
@@ -15,20 +60,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Jhonata Nogueira"
-            content="Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Rerum repellat corporis praesentium quasi est 
-            dolor nam ipsum sit, aperiam magnam voluptas veniam doloremque! 
-            Ab enim, dicta maxime a nisi molestiae"
-          />
-          <Post
-            author="Luiz Gustavo"
-            content="Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Rerum repellat corporis praesentium quasi est 
-            dolor nam ipsum sit, aperiam magnam voluptas veniam doloremque! 
-            Ab enim, dicta maxime a nisi molestiae"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
 
       </div>
