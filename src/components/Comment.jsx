e0a7ec521663ed69010f4ca65e7fb8e944a188b5
Jhonata Nogueira <jhonata@flashnetbrasil.com.br>
2022-08-31 21:27:55 -0300

    Componente Avatar
    Foto de Perfil

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,14 @@
 import { Trash, ThumbsUp } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/jhoownogueira.png" />
+            <Avatar
+                src="https://github.com/jhoownogueira.png" 
+                hasBorder={false}
+            />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,9 +7,8 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img 
-                    className={styles.avatar} 
-                    src="https://github.com/jhoownogueira.png" 
+                    <Avatar 
+                    src={'https://avatars.githubusercontent.com/u/101819736?v=4'}
                     />
                     <div className={styles.authorInfo}>
                         <strong>Jhonata Nogueira</strong>
